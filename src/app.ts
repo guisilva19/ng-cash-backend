4fb5e8d6fc126ba3173472493ca70af764860fd1
Guilherme Silva <2003silvagui@gmail.com>
2022-11-17 18:45:30 -0300

    fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import express from "express";
 import handleError from "./middlewares/handleError.middleware";
 import AppRoutes from "./routes";
 
+const cors = require("cors");
 const app = express()
 
+app.use(cors)
 app.use(express.json())
 AppRoutes(app)
 app.use(handleError)
