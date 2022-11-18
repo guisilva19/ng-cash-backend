8da8e5989cf194aabd070216c8821d07204fa552
Guilherme Silva <2003silvagui@gmail.com>
2022-11-18 12:09:37 -0300

    fix: corringo cors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-const cors = require("cors");
 import "reflect-metadata"
 import "express-async-errors"
 import express from "express";
 import handleError from "./middlewares/handleError.middleware";
 import AppRoutes from "./routes";
 
+const cors = require("cors");
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 AppRoutes(app)
-app.use(cors)
 app.use(handleError)
 
 
