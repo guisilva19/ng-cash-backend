a4475a55172ef8e12d610f98baeb19d56b91cadb
Guilherme Silva <2003silvagui@gmail.com>
2022-11-18 12:54:07 -0300

    feat: routes get user

--- src/routes/users/userRoutes.ts
+++ src/routes/users/userRoutes.ts
@@ -4,12 +4,16 @@ import createUserController from "../../controllers/users/createUser.controller"
 import sessionUserController from "../../controllers/users/sessionUser.controller";
 import verifyCreateUserSerializer from "../../serializers/verifyCreateUser.serializer";
 import verifySessionSerializer from "../../serializers/verifySession.serializer";
+import authTokenMiddleware from "../../middlewares/authToken.middeware";
+import userController from "../../controllers/users/user.controller";
 
 const routes = Router()
 
 const userRoutes = () => {
     routes.post('/register', verifyCreateUserSerializer(schemaRegister), createUserController)
     routes.post('/login', verifySessionSerializer(schemaSession), sessionUserController)
+    routes.get('/user', authTokenMiddleware, userController)
+    
     return routes
 }
 
